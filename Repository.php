14c748f527cf91392c1da3ded13e8c91bd68eff0
guzzilar <iam.nuttanon@gmail.com>
2017-01-15 17:24:45 +0700

    OmisePlugin\Repository:: rename method create() to add(), and implement create() method to allow user to create new repository without assign current object to the repository

--- Repository.php
+++ Repository.php
@@ -28,14 +28,31 @@ class Repository
     /**
      * Initiate Repository instance.
      *
+     * @param  string $context_name
+     * @param  mixed  $param
+     *
+     * @return \OmisePlugin\Repository
+     */
+    public static function create($context_name, $params)
+    {
+        $repository = new self(new Context, null);
+        $repository->attachContextByName($context_name);
+        $repository->object = $repository->context->execute('create', array('params' => $params));
+
+        return $repository;
+    }
+
+    /**
+     * Initiate Repository instance.
+     *
      * @param  object $object
      *
      * @return \OmisePlugin\Repository
      */
-    public static function create($object)
+    public static function add($object)
     {
         $repository = new self(new Context, $object);
-        $repository->attachContext($object);
+        $repository->attachContextByObject($object);
 
         return $repository;
     }
@@ -43,9 +60,17 @@ class Repository
     /**
      * @param object $object
      */
-    public function attachContext($object)
+    public function attachContextByObject($object)
     {
         $this->context->readFromObject($object);
+    }
+
+    /**
+     * @param string $name
+     */
+    public function attachContextByName($name)
+    {
+        $this->context->readFromName($name);
     }
 
     /**
